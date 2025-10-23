bae258ac53115002a4a7d7a81abb154f1a0c511b
Your Name <your.email@example.com>
2025-10-23 22:28:04 +0530

    added duplicate pojo with commented

--- src/main/java/com/example/manyToMany/entity/BusStop.java
+++ src/main/java/com/example/manyToMany/entity/BusStop.java
@@ -57,4 +57,37 @@ public class BusStop {
 
 // added comment in busStop to check alll the process for pull reque
 
+//
+//package com.example.manyToMany.entity;
+//
+//import jakarta.persistence.*;
+//        import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.time.LocalDateTime;
+//
+//@Getter
+//@Setter
+//@Entity
+//@Table(name = "bus_stop")
+//public class BusStop {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", nullable = false)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "bus_id")
+//    private Bus bus;
+//
+//    @ManyToOne
+//    @JoinColumn(name="stop_id")
+//    private Stop stop;
+//
+//    @Column(name="order_Number",nullable = false)
+//    private Integer orderNumber;
+//
+//    @Column(name="deparature_time",nullable = false)
+//    private LocalDateTime deparatureTime;
 
+//}git
